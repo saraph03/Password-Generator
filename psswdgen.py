65e4ae01035310a071a86645b69dd3f0e1815901
Sara Phondge <117596793+saraph03@users.noreply.github.com>
2023-07-27 13:18:07 -0400

    Update psswdgen.py

--- psswdgen.py
+++ psswdgen.py
@@ -3,12 +3,19 @@ import random
 
 def password_generator():
 # defining what the password will include
+    num_length = int(input("Enter an integer: "))
+    if num_length < 8:
+        print("Not Valid Length. Password length should be at least 8 characters")
+        return None
     lowercase = string.ascii_lowercase
     uppercase = string.ascii_uppercase
     num = string.digits
     special_char = string.punctuation
+        
+    chars_per_category = num_length // 4
+    remaining_chars = num_length % 4
     #adding the characters 
-    password_char = random.sample(lowercase, 2) + random.sample(uppercase, 2) + random.sample(num, 2) + random.sample(special_char, 2)
+    password_char = random.sample(lowercase, chars_per_category) + random.sample(uppercase, chars_per_category) + random.sample(num, chars_per_category) + random.sample(special_char, chars_per_category + remaining_chars)
     #shuffling        
     random.shuffle(password_char) 
     #generating the random password
